81a81767f61eb0d2c324b1ea8dd711a0e2f7401a
dschwerdel <daniel.schwerdel@gmail.com>
2021-10-08 19:30:43 +0200

    Last Version so far, all seams to be fixed

--- 1.salestax/salestax.rs
+++ 1.salestax/salestax.rs
@@ -77,14 +77,12 @@ fn build_invoice (arr: &[Inputs]) {
 		if arr[i].txt.contains("import") {
 				
 			// ... +5% on sales price
-
-			let itax = arr[i].prc * 0.05;
+			let mut itax = arr[i].prc * 0.05;
+			// round to nearest .05
+			// Fix because .56.. was always rounded down
+			itax = ( (itax * 10.00 ).ceil() ) / 10.00; 
+			itax = (itax / 0.05).round() * 0.05;
 			prc = prc + itax ;
-		/*	println!("> {}", itax);
-			let rond = (arr[i].prc * 100.0).round() / 100.0;
-			println!("> {}", rond);
-			
-		*/
 		}
 
 		// The Tax is the substract of Brutto - Netto
@@ -102,42 +100,4 @@ fn build_invoice (arr: &[Inputs]) {
 	}
 
 }
-
-/*
-Input   1:
->   1   book   at   12.49        
->   1   music   CD   at   14.99        
->   1   chocolate   bar   at   0.85    
-
-Input   2:    
->   1   imported   box   of   chocolates   at   10.00        
->   1   imported   bottle   of   perfume   at   47.50        
-  
-Input   3:    
->   1   imported   bottle   of   perfume   at   27.99        
->   1   bottle   of   perfume   at   18.99        
->   1   packet   of   headache   pills   at   9.75        
->   1   box   of   imported   chocolates   at   11.25   
-
-Output   1:    
->   1   book:   12.49        
->   1   music   CD:   16.49        
->   1   chocolate   bar:   0.85        
->   Sales   Taxes:   1.50        
->   Total:   29.83        
-  
-Output   2:    
->   1   imported   box   of   chocolates:   10.50        
->   1   imported   bottle   of   perfume:   54.65        
->   Sales   Taxes:   7.65        
->   Total:   65.15    
-  
-Output   3:    
->   1   imported   bottle   of   perfume:   32.19        
->   1   bottle   of   perfume:   20.89        
->   1   packet   of   headache   pills:   9.75        
->   1   imported   box   of   chocolates:   11.85        
->   Sales   Taxes:   6.70        
->   Total:   74.68  
-*/
 
